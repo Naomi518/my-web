2ff40878870afe2b8a6e6b49521d732169fa43b9
Naomi518 <Oyk_bumbum2115@icloud.com>
2025-01-27 12:25:02 +0100

    Add files via upload

---
+++ js app.2.js
@@ -0,0 +1,9 @@
+// Simple form submission message
+<script>
+document.getElementById("Oyinkansola")
+var email=document.getElementById[Hephzibahnaomi48@gmail.com]
+.addEventListener("submit", function(event) 
+    event.preventDefault();
+    alert("Thank you for your message!Oyinkansola,I will get back to you at,Hephzibahnaomi48@gmail.com");
+    document.getElementById("contactForm").reset();
+    </script>
